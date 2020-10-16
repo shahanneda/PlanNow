ad6a2b53d10b9de4bbbf0012057d3054027b8039
Shahan Neda <shahan.neda@gmail.com>
2020-10-15 21:45:48 -0700

    updated domain

--- client/src/js/main.jsx
+++ client/src/js/main.jsx
@@ -6,7 +6,7 @@ const wrapper = document.getElementById("container");
 
 ReactDOM.render(
         <div>
-          <NowPlan url="http://18.216.129.53/plannow"/>
+          <NowPlan url="http://shahanneda.xyz/plannow"/>
         </div>
 , wrapper);
 
